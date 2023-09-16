7273217ef7f6c34611ce466d3ab58d4b8f916219
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-09-16 20:54:28 +0600

    Delete system.js

--- system.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the refresh icon element
-const refreshIcon = document.getElementById('refresh-icon');
-
-// Add a click event listener to the icon
-refreshIcon.addEventListener('click', function () {
-    // Reload the page when the icon is clicked
-    location.reload();
-});
