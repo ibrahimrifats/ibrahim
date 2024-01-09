f63e379943b2e555a04aa860f53aee1cc2656e13
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2024-01-10 01:46:26 +0600

    Update pl.js

--- pl.js
+++ pl.js
@@ -1,1 +1,18 @@
+        // Counter variable
+        let visitorCount = 0;
 
+        // Function to increment the counter
+        function incrementCounter() {
+            visitorCount++;
+            updateCounterDisplay();
+        }
+
+        // Function to update the counter display in the navbar
+        function updateCounterDisplay() {
+            document.getElementById('visitor-count').innerText = `Visitors: ${visitorCount}`;
+        }
+
+        // Increment the counter when the page is loaded
+        window.onload = function () {
+            incrementCounter();
+        };
