79ab466253557a1c79d4904728a356d3ef751bbe
ibrahimrifatcse <rifatinto@gmail.com>
2024-03-19 00:58:49 +0600

    change full website

--- Code-Editor-main/main.js
+++
@@ -1,9 +0,0 @@
-// Create and configure the code editors for each editor container
-const editors = document.querySelectorAll(".editor");
-
-editors.forEach((editor, index) => {
-  ace.edit(editor, {
-    theme: "ace/theme/cobalt",
-    mode: "ace/mode/javascript",
-  });
-});
